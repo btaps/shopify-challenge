e7be55fa412e84a1e729eb0cd9a31e1dc8ad1451
Bruno <btapiasierra@outlook.com>
2021-05-09 22:20:08 -0700

    Updated README, minor changes

--- src/App.js
+++ src/App.js
@@ -17,11 +17,17 @@ import {
   setSignOutState,
 } from "./features/user/userSlice";
 import Nominations from "./components/Nominations";
+import {
+  selectNominations,
+  selectSearchResults,
+} from "./features/movie/movieSlice";
 
 function App() {
   const dispatch = useDispatch();
   const userName = useSelector(selectUserName);
   const userPhoto = useSelector(selectUserPhoto);
+  const nominations = useSelector(selectNominations);
+  const searchResults = useSelector(selectSearchResults);
 
   useEffect(() => {
     auth.onAuthStateChanged(async (user) => {
@@ -75,6 +81,11 @@ function App() {
           <Intro handleAuth={handleAuth} />
         ) : (
           <>
+            {nominations.length === 0 && searchResults.length === 0 && (
+              <Container>
+                <Message>Try searching for a movie!</Message>
+              </Container>
+            )}
             <Nominations />
             <Results />
           </>
@@ -94,4 +105,12 @@ const Main = styled.div`
   z-index: 0;
 `;
 
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 60vh;
+`;
+
+const Message = styled.h1``;
 export default App;
